e8b8edb5d73ffabe118e221ddde4b5678db067e3
Cesar <cesarmiguel199@gmail.com>
2020-10-09 17:19:20 +0200

    Updates

---
+++ precache-manifest.612c29d68d430b94018d2bcccaceff1a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72631320b7af708f2e6dcb62f647322",
+    "url": "/react-Comunity-Service/index.html"
+  },
+  {
+    "revision": "e39e571eb07a90eaab85",
+    "url": "/react-Comunity-Service/static/css/2.5bf33b3b.chunk.css"
+  },
+  {
+    "revision": "566c8ec63c7e942dfbc3",
+    "url": "/react-Comunity-Service/static/css/main.1f48cbd5.chunk.css"
+  },
+  {
+    "revision": "e39e571eb07a90eaab85",
+    "url": "/react-Comunity-Service/static/js/2.2885a628.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/react-Comunity-Service/static/js/2.2885a628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566c8ec63c7e942dfbc3",
+    "url": "/react-Comunity-Service/static/js/main.82b647fe.chunk.js"
+  },
+  {
+    "revision": "3609d4d3f468820d39f3",
+    "url": "/react-Comunity-Service/static/js/runtime-main.1bb306bb.js"
+  },
+  {
+    "revision": "a600791112cf53384768d67629a94e04",
+    "url": "/react-Comunity-Service/static/media/bg-1.a6007911.png"
+  },
+  {
+    "revision": "377e727976860b5da5888302802a019a",
+    "url": "/react-Comunity-Service/static/media/bg-2.377e7279.png"
+  },
+  {
+    "revision": "3dc6eb74a8675d947082460645072bda",
+    "url": "/react-Comunity-Service/static/media/bg.3dc6eb74.jpg"
+  },
+  {
+    "revision": "b7340a701ce030215d32627a4d4542fb",
+    "url": "/react-Comunity-Service/static/media/home-img.b7340a70.png"
+  },
+  {
+    "revision": "4570c307e0a3d29c7b18854540b2a4cf",
+    "url": "/react-Comunity-Service/static/media/img-2.4570c307.png"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/react-Comunity-Service/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/react-Comunity-Service/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/react-Comunity-Service/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/react-Comunity-Service/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  }
+]);
